ab5cdc51d62f96b6aea54cad7f755b03afbbfce2
XaXaTuSHeCH <eg03or@mail.ru>
2025-02-09 21:23:24 +0700

    main branch fix

--- Task1/task1.cpp
+++ Task1/task1.cpp
@@ -1,22 +1,25 @@
 #include <iostream>
+#include <iomanip>
 #include <cmath>
 
-#ifndef DATA_TYPE
-#define DATA_TYPE double
+#ifndef TYPE
+    #define TYPE double
 #endif
+
+using namespace std;
 
 constexpr size_t N = 10000000;
 
 int main() {
-    DATA_TYPE* values = new DATA_TYPE[N];
+    TYPE* values = new TYPE[N];
     for (size_t i = 0; i < N; ++i) {
-        values[i] = std::sin((static_cast<double>(i) / N) * 2 * M_PI);
+        values[i] = sin((static_cast<double>(i) / N) * 2 * M_PI);
     }
-    DATA_TYPE sum = 0;
+    TYPE sum = 0;
     for (size_t i = 0; i < N; ++i) {
         sum += values[i];
     }
     delete[] values;
-    std::cout << sum << std::endl;
+    cout << sum << endl;
     return 0;
 }
